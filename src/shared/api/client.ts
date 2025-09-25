2f5e0f3ffa8ef2c4f5ce511b2332b140c760635f
Matvei <motya2222@gmail.com>
2025-09-25 13:18:07 +0300

    feat: add post method to client

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -10,4 +10,26 @@ export const client = {
 
     return response.json();
   },
+  async post(
+    endpoint: string,
+    body: any,
+    errorMessage: string,
+    options?: RequestInit,
+  ) {
+    const response = await fetch(`${BACKEND_BASE_URL}${endpoint}`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      ...options,
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        ...options?.headers,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(errorMessage);
+    }
+
+    return response.json();
+  },
 };
