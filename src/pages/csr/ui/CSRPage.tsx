e4607a535c0351d4d241e65f38b808550d6a6324
Matvei <motya2222@gmail.com>
2025-09-26 10:53:37 +0300

    fix: swap main tag to fragment

--- src/pages/csr/ui/CSRPage.tsx
+++ src/pages/csr/ui/CSRPage.tsx
@@ -7,9 +7,9 @@ export default function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <main>
+    <>
       <p>{count} likes</p>
       <Button onClick={() => setCount(count + 1)}>Click me</Button>
-    </main>
+    </>
   );
 }
